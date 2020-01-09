6a263b0f4e661a9be8f2a83fd797c27814667fce
yangbin.zqgame <bin.yang@zqgame.com>
2020-01-09 18:16:36 +0800

    图形应用

--- assets/Scripts/Menu.ts
+++ assets/Scripts/Menu.ts
@@ -4,14 +4,18 @@ const { property, ccclass } = cc._decorator;
 @ccclass
 export default class MenuPanel extends cc.Component {
 
+    @property( cc.Node )
+    mask: cc.Node = null;
+
     protected onLoad() {
-        // let mask = new cc.Graphics();
-        // mask.fillColor = cc.color( 0, 0, 0, 0.7 );
-        // mask.roundRect( 0, 0, 250, 200, 50 );
 
-        // this.node.addChild( mask );
-
+        let ctx = this.mask.getComponent( cc.Graphics );
+        ctx.roundRect( 0, 0, 250, 200, 50 );
+        ctx.fill();
+        // this.mask.x = this.node.width / 2 - this.mask.width / 2;
+        // this.mask.y = this.node.height / 2 - this.mask.height / 2;
         // graphics.strokeColor = cc.color( 233, 186, 18 );
+
 
     }
 
